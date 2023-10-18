f96ba2f998fd96e831cb408807fa4333377cc73f
Srianantha34 <148346048+Srianantha34@users.noreply.github.com>
2023-10-18 20:38:21 +0800

    Update data-h2.sql

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -5,14 +5,15 @@ insert into movie values (3,'Action','English','2023-10-07','SCREAM');
 insert into movie values (4,'Action','Hindi','2023-10-07','RAAZ');
 
 
+--cinema
 insert into cinema values (1,'Street 1','Bangalore','CINEPOL1');
 insert into cinema values (2,'Street 1','Bangalore','CINEPOL2');
 
+--cinema_hall
 insert into cinema_hall values (60,1,1,'HALL1');
 insert into cinema_hall values (62,2,2,'HALL2');
 
 --    -- show table --
---INSERT INTO show VALUES (1,'2023-10-17 09:00:00',1,1,'2023-10-17 07:00:00');
 INSERT INTO show VALUES (2,'2023-10-17 11:00:00',1,2,'2023-10-17 09:00:00');
 INSERT INTO show VALUES (2,'2023-10-17 09:00:00',2,3,'2023-10-17 07:00:00');
 
@@ -24,8 +25,8 @@ insert into seat values (1,4);
 insert into seat values (1,5);
 
 ----seatcinemahall
-insert into CINEMA_HALL_SEATS values (1,1);
-insert into CINEMA_HALL_SEATS values (1,2);
-insert into CINEMA_HALL_SEATS values (1,3);
-insert into CINEMA_HALL_SEATS values (1,4);
-insert into CINEMA_HALL_SEATS values (1,5);
+insert into CINEMA_HALL_SEATS values (2,1);
+insert into CINEMA_HALL_SEATS values (2,2);
+insert into CINEMA_HALL_SEATS values (2,3);
+insert into CINEMA_HALL_SEATS values (2,4);
+insert into CINEMA_HALL_SEATS values (2,5);
